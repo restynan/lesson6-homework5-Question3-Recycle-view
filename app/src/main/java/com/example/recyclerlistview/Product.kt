619cbbb8dd66f2cb681f2af6851c8087da51b812
610501 <restynan@gmail.com>
2019-12-02 22:58:33 -0600

    lesson6-homework5-Question3-recycleView

---
+++ app/src/main/java/com/example/recyclerlistview/Product.kt
@@ -0,0 +1,5 @@
+package com.example.recyclerlistview
+
+data class Product(val title:String,val price:Double,val color:String,val imageName:Int,val itemid:String="", val descProduct:String="'"){
+
+}
